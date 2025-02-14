ccac8b2103edb105bbafd523a9c5ad455113e228
das-globally-web <das.globally.web@gmail.com>
2025-02-14 18:58:54 +0530

    changes

--- lib/trendingskills/controller/sikllscontroller.dart
+++ lib/trendingskills/controller/sikllscontroller.dart
@@ -1,13 +1,21 @@
+import 'package:educationapp/CORE/api_controller.dart';
 import 'package:educationapp/config/preety.dio.dart';
 import 'package:educationapp/collegeReviews/model/allmentors.model.dart';
 import 'package:educationapp/trendingskills/controller/service/skills.service.dart';
 import 'package:educationapp/trendingskills/model/new.skills.model.dart';
 import 'package:educationapp/trendingskills/model/skills.model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final apiSkillClientProvider = FutureProvider<SkillService>((ref) async {
+  final dio = await ref.watch(dioProvider.future);
+  return SkillService(dio);
+});
+
 final skilssProvide = FutureProvider<SkillsModel>((ref) async {
-  final service = SkillService(await createDio());
-  return service.getALLSiklls();
+  final client = await ref.watch(apiSkillClientProvider.future);
+
+  return await compute(ApiController.getAllSkilss, client);
 });
 
 final newskillsProvider =

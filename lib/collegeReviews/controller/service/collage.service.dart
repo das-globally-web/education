46bc195ecd341d68f1ad40c536172c5ffeffa784
das-globally-web <das.globally.web@gmail.com>
2025-03-06 14:52:06 +0530

    cadasd

--- lib/collegeReviews/controller/service/collage.service.dart
+++ lib/collegeReviews/controller/service/collage.service.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:educationapp/collegeReviews/model/allmentors.model.dart';
 import 'package:educationapp/collegeReviews/model/perticuler.collage.model.dart';
-import 'package:educationapp/home/model/mentors.model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part "collage.service.g.dart";
